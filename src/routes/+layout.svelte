98a0f6b482ad9e41b304b16b7fbc44990c9d2eef
dpointeck <daniel.pointecker@gmail.com>
2023-11-01 13:22:46 +0100

    Add tailwind formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
-    import "../tailwind.css";
-  </script>
-  
-  <slot />
+	import '../tailwind.css';
+</script>
+
+<header class="mt-3 text-center">
+	<h1 class="text-2xl font-bold">Tabata</h1>
+</header>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const settings = {
+		prepare: 300,
+		work: 20,
+		rest: 10,
+		cycles: 8,
+		sets: 1
+	};
+</script>
+
+<div class="mt-4 px-4">
+	{#each Object.keys(settings) as key}
+		<div class="flex justify-between">
+			<label for={key}>{key}</label>
+			<input type="number" id={key} bind:value={settings[key]} class="w-1/2" />
+		</div>
+	{/each}
+</div>
